fc355b161e876a4c68600ca7408ee48e60dd0c8b
SiuLaw <lawsiufung@me.com>
2018-02-27 14:49:09 +0000

    numeric and analytic deltas

--- asian.cpp
+++ asian.cpp
@@ -253,8 +253,8 @@ public :
 		  MC_euler_pricing(S0, r, v, no_sims, res);
 	  }
 	  else if (icompare(method, "euler_na")) {
-		  MC_euler_pricing_non_anti(S0, r, v, no_sims, res);
-	}else if(icompare(method,"milstein")){
+          MC_euler_pricing_non_anti(S0, r, v, no_sims, res);
+      }else if(icompare(method,"milstein")){
 
     }else if(icompare(method,"analytic")){
 		analytic_solution_pricing(S0, r, v, res);
@@ -266,8 +266,116 @@ public :
   }
 
 	// this function calculate delta at time 0 and return analytic statistics
-	vector<double> calculate_delta(string method,int no_sims = 100000);
+    vector<double> calculate_delta(string method, string method2, double S0, double r, double v, int no_sims = 100000, double h = 0.01) {
+        // method2 is the user choice of between finite difference/ pathwise/ likelihood ratio
+        vector<double> delta;
+        
+        if(icompare(method2, "fd")) {
+            MC_fd_delta(method, S0, r, v, no_sims, delta, h);
+        } else if(icompare(method2, "pw")) {
+            MC_pw_delta(method, S0, r, v, no_sims, delta);
+        } else if(icompare(method2, "analytic") ) {
+            analytic_solution_delta(S0,r,v,delta);
+        }else {
+            // edge case:
+        }
+        
+        return delta;
+    };
 
+    void MC_fd_delta(string method, double S0, double r, double v, int no_sims, vector<double> &delta, double h) {
+        
+        clock_t c;
+        double duration;
+        c = clock();// start the timer
+        
+        
+        
+        // option prices with initial stock price S0-h and S0+h
+        vector<double> res1 = calculate_price(method, S0+h, r, v, no_sims);
+        vector<double> res2 = calculate_price(method, S0-h, r, v, no_sims);
+        
+        double price1 = res1[0];
+        double price2 = res2[0];
+        
+        
+        // record time
+        duration = (clock() - c) / (double)CLOCKS_PER_SEC;
+        
+        delta.push_back( ((price2 - price1) / (2 * h)) );
+        delta.push_back(duration);
+    }
+    
+    void MC_pw_delta(string method, double S0, double r, double v, int no_sims, vector<double> &delta) {
+        clock_t c;
+        double duration;
+        c = clock();// start the timer
+        
+        // prameter initlisation
+        clock_t t;
+        double price = 0;
+        double dt = (double)T/N;
+        double dt_sqr = pow((double)T/N,0.5); // ADDED: Casting T to double
+        double mean_sqr = 0;
+        double sum = 0;
+        
+        // run the simulation, NO variance reduction
+        for (unsigned int i = 0; i < no_sims; i++) {
+            vector<double> z = normal_generator(N); //generate normal vector of size N
+            double log_sum = log(S0);
+            double s = S0;
+            
+            for (unsigned int j = 0;j < N;j++) {
+                s += r * s*dt + v * s*dt_sqr*z[j];
+                log_sum += log(s);
+            }
+            double average = exp(log_sum / (N + 1));
+            bool indicator = average > K;
+            if( indicator ) {
+                sum += average / S0;
+                mean_sqr += pow( average / S0,2);
+            }
+        }
+        // record time
+        duration = (clock()-t)/(double)CLOCKS_PER_SEC;
+        // return results
+        // handle edge cases
+        delta.push_back((sum/no_sims)); // price
+        delta.push_back(duration); //time
+        delta.push_back(sum/no_sims); //mean
+        delta.push_back((mean_sqr/no_sims)-pow((sum/no_sims),2)); //variance
+
+        
+        // record time
+        duration = (clock() - c) / (double)CLOCKS_PER_SEC;
+        
+        delta.push_back(0);
+        delta.push_back(duration);
+        
+    }
+    
+    void analytic_solution_delta(double S0, double r,double sigma, vector<double> &delta) {
+        clock_t c;
+        double duration;
+        c = clock(); // start the timer
+        
+        double t = 0;
+        
+        double G_t = S0;
+        double mu_bar = (r - sigma * sigma / 2) * pow(T - t, 2) / (2 * T);
+        double sigma_bar = sqrt(sigma*sigma / (T*T) * pow(T - t, 3) / 3);
+        double d2 = 1.0 / sigma_bar * (t / T * log(G_t) + (T - t) / T * log(S0) + mu_bar - log(K));
+        double d1 = d2 + sigma_bar;
+        
+        double delta_val = exp(mu_bar + pow(sigma_bar, 2) / 2)* normalCDF(d1) + exp(mu_bar + pow(sigma_bar, 2) / 2) / sigma_bar * normalPDF(d1) - K / (sigma_bar * S0) * normalPDF(d2);
+        
+        // record time
+        duration = (clock() - c) / (double)CLOCKS_PER_SEC;
+        
+        delta.push_back( delta_val );
+        delta.push_back( duration );
+    };
+    
 	// this function calculate gamma at time 0 and return analytic statistics
 	vector<double> calculate_gamma(string method,int no_sims = 100000);
 
@@ -283,46 +391,35 @@ int main() {
 	double s0 = 100;
 	double r = 0.05;
 	double v = 0.4;
-	int no_sims = 1000;
+	int no_sims = 100000;
+    double h = 0.1;
+    vector<double> res, delta;
+    
 
 	srand(time(NULL));
 	asian_option_geometric opt(type,T,N,K);
-	vector<double> res = opt.calculate_price("analytic", s0, r, v);
-
+    res = opt.calculate_price("analytic", s0, r, v);
+    
 	cout << "Analytic: " << endl;
 	res_print(res);
-	cout << endl;
-	
-	cout << "n = " << no_sims << endl;
-	cout << "Euler: " << endl;
-	res = opt.calculate_price("euler", s0, r, v, no_sims);
-	res_print(res);
-	cout << endl;
-	cout << "Euler NA: " << endl;
-	res = opt.calculate_price("euler_na", s0, r, v, no_sims);
-	res_print(res);
+    delta = opt.calculate_delta("analytic","analytic", s0, r,v);
+    cout << "delta = " << delta[0] << endl;
 	cout << endl;
 
-	no_sims = 10000;
+    
+    
 	cout << "n = " << no_sims << endl;
-	cout << "Euler: " << endl;
-	res = opt.calculate_price("euler", s0, r, v, no_sims);
-	res_print(res);
-	cout << endl;
 	cout << "Euler NA: " << endl;
-	res = opt.calculate_price("euler_na", s0, r, v, no_sims);
+    
+    res = opt.calculate_price("euler_na", s0, r, v, no_sims);
 	res_print(res);
-	cout << endl;
-
-	no_sims = 100000;
-	cout << "n = " << no_sims << endl;
-	cout << "Euler: " << endl;
-	res = opt.calculate_price("euler", s0, r, v, no_sims);
-	res_print(res);
-	cout << endl;
-	cout << "Euler NA: " << endl;
-	res = opt.calculate_price("euler_na", s0, r, v, no_sims);
-	res_print(res);
+    
+    delta = opt.calculate_delta("euler_na", "fd", s0, r,v , no_sims, h);
+    cout << "delta_fd = " << delta[0] << endl;
+    
+    delta = opt.calculate_delta("euler_na", "pw", s0, r, v, no_sims);
+    cout << "delta_pw = " << delta[0] << endl;
+    
 	cout << endl;
 
 	int dummy;
